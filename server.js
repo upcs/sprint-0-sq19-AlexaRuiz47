c77a33296ac858ceab0468e61987cbe90aab1e55
AlexaRuiz47 <31424225+AlexaRuiz47@users.noreply.github.com>
2019-01-29 11:24:55 -0800

    Update server.js
    retrying server.js

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 var http = require('http');
 var fs = require('fs');
-var url = require('url')
+var url = require('url');
 
-http.createServer( function (request, response){
-	var pathname = url.parse(request.url).pathname; 
-	console.log("Request for " + pathname+ " recieved."); 
-	fr.readFile(pathname.substr(1), function (err,data){
-		if(err){
-			console.log(err); 
-			response.writeHead(404, {'Content-Type': 'text/html'})}
-	
-		else{
-
-			response.writeHead(200, {'Content-Type':'text/html'})
-			response.write(data.toString());
-		}
-
-		response.end(); 
-
-		});
+// Create a server
+http.createServer( function (request, response) {  
+   // Parse the request containing file name
+   var pathname = url.parse(request.url).pathname;
+   
+   // Print the name of the file for which request is made.
+   console.log("Request for " + pathname + " received.");
+   
+   // Read the requested file content from file system
+   fs.readFile(pathname.substr(1), function (err, data) {
+      if (err) {
+         console.log(err);
+         
+         // HTTP Status: 404 : NOT FOUND
+         // Content Type: text/plain
+         response.writeHead(404, {'Content-Type': 'text/html'});
+      } else {	
+         //Page found	  
+         // HTTP Status: 200 : OK
+         // Content Type: text/plain
+         response.writeHead(200, {'Content-Type': 'text/html'});	
+         
+         // Write the content of the file to response body
+         response.write(data.toString());		
+      }
+      
+      // Send the response body 
+      response.end();
+   });   
 }).listen(8081);
 
-console.log('Server running at http://127.0.01:8081/');
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
