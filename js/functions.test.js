fe4027c39d5791953460520545886de1aad2e0f2
AlexaRuiz47 <31424225+AlexaRuiz47@users.noreply.github.com>
2019-01-29 15:53:43 -0800

    Update functions.test.js

--- js/functions.test.js
+++ js/functions.test.js
@@ -3,3 +3,8 @@ const sum = require('./functions');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+
+test('adds 6 + 3 to equal 9', () => {
+  expect(sum 6, 3)).toBe(9);
+});
